6dc99e8012b0a4ba9877e6ba04c984669fb98827
Richard Doe <richard.doe@rwdit.net>
2013-12-17 16:47:23 +0000

    EY: Shared application config file and public uploads dir.

--- deploy/before_symlink.rb
+++ deploy/before_symlink.rb
@@ -1,3 +1,6 @@
+# Symlink to shared application config file
+run "ln -nfs #{config.shared_path}/config/application.ini #{config.release_path}/application/config/application.ini"
+
 # Create & symlink shared cache dir
 run "mkdir -p #{config.shared_path}/cache"
 run "ln -nfs #{config.shared_path}/cache #{config.release_path}/cache"
@@ -6,3 +9,7 @@ run "ln -nfs #{config.shared_path}/cache #{config.release_path}/cache"
 run "mkdir -p #{config.shared_path}/data/uploads"
 run "rmdir #{config.release_path}/data/uploads"
 run "ln -nfs #{config.shared_path}/data/uploads #{config.release_path}/data/uploads"
+
+# Create & symlink shared public uploads dir
+run "mkdir -p #{config.shared_path}/public/data/icons/uploads"
+run "ln -nfs #{config.shared_path}/public/data #{config.release_path}/public/data"
