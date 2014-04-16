3faabe67a3893a10950b9def62d50017f02cca2a
Richard Doe <richard.doe@rwdit.net>
2014-04-16 13:13:57 +0100

    Update EY deploy hook to account for empty data storage directories.

--- deploy/before_symlink.rb
+++ deploy/before_symlink.rb
@@ -3,14 +3,16 @@ run "ln -nfs #{config.shared_path}/config/application.ini #{config.release_path}
 
 # Create & symlink shared cache dir
 run "mkdir -p #{config.shared_path}/cache"
+run "rm -r #{config.release_path}/cache"
 run "ln -nfs #{config.shared_path}/cache #{config.release_path}/cache"
 
 # Create & symlink shared uploads dir
 run "mkdir -p #{config.shared_path}/data/uploads"
-run "rmdir #{config.release_path}/data/uploads"
+run "rm -r #{config.release_path}/data/uploads"
 run "ln -nfs #{config.shared_path}/data/uploads #{config.release_path}/data/uploads"
 
 # Create & symlink shared public uploads dir
 run "mkdir -p #{config.shared_path}/public/data/icons/uploads"
 run "mkdir -p #{config.shared_path}/public/data/icons/assigned"
+run "rm -r #{config.release_path}/public/data"
 run "ln -nfs #{config.shared_path}/public/data #{config.release_path}/public/data"
